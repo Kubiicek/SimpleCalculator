0b560daed15a0394af6a6501d85673acac3499f8
Danillo Vieira <asusdanillo@gmail.com>
2024-10-17 18:59:37 -0300

    Calculadora Feita

---
+++ SimpleCalculator/src/entites/Proceed.java
@@ -0,0 +1,85 @@
+package entites;
+
+import view.JApplication;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Proceed {
+	private static Integer TheFirstNumber = null;
+
+	    private final List<String> listExpression;
+
+	    public Proceed() {
+	        this.listExpression = new ArrayList<>();
+	    }
+
+	    public void addPart(String part) {
+	        listExpression.add(part);
+	    }
+	    
+	    public void clean() {
+	    	listExpression.clear();
+	    }
+	    
+	    public void removeLastPart() {
+	    	if (!listExpression.isEmpty()) {
+	    		listExpression.remove(listExpression.size() - 1);
+	    	}
+	    }
+
+	    public String getExpression() {
+	        return String.join("", listExpression);
+	    }
+
+	    public double calculateResult() {
+	        String expression = getExpression();
+	        return evaluteExpression(expression);
+	    }
+
+	    private double evaluteExpression(String expression) {
+	        expression = expression.replaceAll("\s", "");
+	        double result = 0;
+	        char operation = '+';
+	        StringBuilder currentNumber = new StringBuilder();
+
+	        for (int i = 0; i < expression.length(); i++) {
+	            char c = expression.charAt(i);
+
+	            if (Character.isDigit(c) || c == '.') {
+	                currentNumber.append(c);
+	            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+	                result = applyOperation(result, Double.parseDouble(currentNumber.toString()), operation);
+	                operation = c;
+	                currentNumber.setLength(0);
+	            }
+	        }
+
+	        result = applyOperation(result, Double.parseDouble(currentNumber.toString()), operation);
+	        return result;
+	    }
+
+	    private double applyOperation(double num1, double num2, char operation) {
+	        return switch (operation) {
+	            case '+' -> num1 + num2;
+	            case '-' -> num1 - num2;
+	            case '*' -> num1 * num2;
+	            case '/' -> num1 / num2;
+	            default -> throw new IllegalArgumentException("Invalid Operation: " + operation);
+	        };
+	    }
+	
+	public static int storeNumber(String input) {
+		try {
+			int number = Integer.parseInt(input);
+			return 1;
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+	
+	public void imprimirFeijao() {
+		while (JApplication.value) {
+			System.out.println("");
+		}
+	}
+}
---
+++ SimpleCalculator/src/entites/RoundedButton.java
@@ -0,0 +1,43 @@
+package entites;
+
+import javax.swing.JButton;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Insets;
+
+public class RoundedButton extends JButton {
+
+    private static final int ARC_WIDTH = 20;  
+    private static final int ARC_HEIGHT = 20; 
+
+    public RoundedButton(String label) {
+        super(label);
+        setContentAreaFilled(false);
+        setFocusPainted(false);      
+        setBorderPainted(false);    
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isPressed()) {
+            g.setColor(Color.LIGHT_GRAY); 
+        } else if (getModel().isRollover()) {
+            g.setColor(Color.GRAY);      
+        } else {
+            g.setColor(getBackground());
+        }
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, ARC_WIDTH, ARC_HEIGHT);
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, ARC_WIDTH, ARC_HEIGHT);
+    }
+
+    @Override
+    public Insets getInsets() {
+        return new Insets(10, 20, 10, 20);
+    }
+}
